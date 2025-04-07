a7f6d68647eb680081b3faa42ec8fa436e3e013f
Istvan Kovacs <kovacspitti@gmail.com>
2025-04-07 21:13:18 +0200

    created vial keymap for neito

---
+++ keyboards/neito/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID { 0x8F, 0x0E, 0x97, 0x67, 0xE8, 0xEF, 0x7E, 0xD9 }
+
+/* Vial unlock keycombo: ESC + Return */
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
